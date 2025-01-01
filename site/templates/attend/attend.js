41ce2dc9a3dfddd1d656587b7f506157b434640b
Luciano Pio <pio@boragora.app>
2025-01-01 20:14:41 +0000

    Atualização tabela de emolumentos e correção modelo de impressão

--- site/templates/attend/attend.js
+++ site/templates/attend/attend.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     load_attend()
      {% if config.DEBUG %}
-        open_attend_info('677078ffec0022bcfc1851f7')
+        // open_attend_info('677078ffec0022bcfc1851f7')
     {% endif %}
 })
 
